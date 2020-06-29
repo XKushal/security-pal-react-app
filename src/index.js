d317069f120d405cf3a24062620089e421e0733c
Xkushal <kushalyhakuri123@gmail.com>
2020-06-29 01:13:11 -0500

    imported new css files to integrate in app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './_root.scss';
@@ -13,14 +15,38 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+$("input[type='email']").blur(function() {
+	if ($(this).val() !== '') {
+		$(this).addClass('hasValue');
+	} else {
+		$(this).removeClass('hasValue');
+	}
+});
+
+const config = {
+	api: `${process.env.REACT_APP_API}`,
+	title: 'Contact Me',
+	successMessage: 'Thank you for contcting me.',
+	errorMessage: 'Sorry we have some problems.',
+	fields: {
+		email: '',
+		msg: ''
+	},
+	fieldsConfig: [
+		{
+			label: 'Email',
+			fieldName: 'email',
+			type: 'email',
+			placeholder: ' Your Email',
+			isRequired: true,
+			klassName: 'email-field'
+		}
+	]
+};
 ReactDOM.render(
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
