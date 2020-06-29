745e5ad9fc1d277424c1d09f8574ca18102dfbc1
Xkushal <kushalyhakuri123@gmail.com>
2020-06-29 01:10:55 -0500

    Header file added with some changes

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,25 +1,35 @@
-import React, {Component } from "react";
-import { Navbar,Container, Button } from "react-bootstrap";
+import React, { Component } from 'react';
+import { Navbar, Container, Button } from 'react-bootstrap';
 
-class Header extends Component{
-    render(){
-        return(
-            <Navbar id="header" className="fixed-top ">
-                <div className="container d-flex align-items-center">
-                <h1 className="logo mr-auto "><a className="button-color" href="#">  SecurityPal</a></h1>
-                <a href="#" className="get-started-btn button-color" variant="outline-warning">Contact Us</a>
-
-                </div>
-            </Navbar>
-        );
-    }
+class Header extends Component {
+	render() {
+		return (
+			<Navbar id="header" className="fixed-top">
+				<div className="container d-flex align-items-center">
+					<h1 className="mr-auto ">
+						<a className="button-color" href="#">
+							SecurityPal
+						</a>
+					</h1>
+					<a href="#" className="get-started-btn button-color" variant="outline-warning">
+						Contact Us
+					</a>
+					<a href="#" className="get-started-btn button-color" variant="outline-warning">
+						Careers
+					</a>
+				</div>
+			</Navbar>
+		);
+	}
 }
 
 export default Header;
 
- {/* <Navbar expand="lg" variant="light" bg="light">
+{
+	/* <Navbar expand="lg" variant="light" bg="light">
             <Container>
                 <Navbar.Brand href="#">SecurityPal</Navbar.Brand>
             </Container>
-            </Navbar> */}
-            // mailto:hey@securitypalhq.com
+            </Navbar> */
+}
+// mailto:hey@securitypalhq.com
